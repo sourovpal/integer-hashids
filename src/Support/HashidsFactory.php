1ff405b5f0f798d95c4be891f0f55fda0d7c8a99
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2024-03-26 08:21:54 +0600

    array_get() to Arr::get()

--- src/Support/HashidsFactory.php
+++ src/Support/HashidsFactory.php
@@ -13,6 +13,7 @@
 namespace Amamarul\Hashids\Support;
 
 use Amamarul\Hashids\Support\Hashids\Hashids;
+use Illuminate\Support\Arr;
 
 /**
  * This is the Hashids factory class.
@@ -47,10 +48,10 @@ class HashidsFactory
     protected function getConfig(array $config)
     {
         return [
-            'salt' => array_get($config, 'salt', ''),
-            'length' => array_get($config, 'length', 0),
-            'alphabet' => array_get($config, 'alphabet', ''),
-            'prefix' => array_get($config, 'prefix', null),
+            'salt' => Arr::get($config, 'salt', ''),
+            'length' => Arr::get($config, 'length', 0),
+            'alphabet' => Arr::get($config, 'alphabet', ''),
+            'prefix' => Arr::get($config, 'prefix', null),
         ];
     }
 
